ecd5d71130c6052c190dbdc05d9639d5f3bfb1f3
unknown <immortalnoufal@gmail.com>
2023-08-24 14:02:44 +0530

    my first commit

---
+++ substraction.java
@@ -0,0 +1,11 @@
+package introduction;
+
+public class substraction {
+	public static void main(String[]args) {
+		int a =10;
+		int b =6;
+		int substract = a-b;
+		System.out.println("the result is "+substract);
+	}
+
+}
